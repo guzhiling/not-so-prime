5d1187a3a5924375aeab8011e9009208b1b7d944
Zhiling <37173670+guzhiling@users.noreply.github.com>
2025-02-04 22:47:37 -0500

    Update main.R

--- coding/r_parallel/main.R
+++ coding/r_parallel/main.R
@@ -1,6 +1,6 @@
 args = (commandArgs(TRUE))
 arg1 = as.numeric(args[1])
-arg2 = as.numeric(args[1])
+arg2 = as.numeric(args[2])
 
 # commands using arguments arg1 and arg2
 print(paste('arg1', arg1, 'arg2', arg2))
