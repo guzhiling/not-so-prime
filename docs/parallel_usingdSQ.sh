08b8311164cdb767edd532a08f9178f0136714b8
Zhiling <37173670+guzhiling@users.noreply.github.com>
2025-05-07 21:24:16 -0400

    Update parallel_usingdSQ.sh

--- docs/parallel_usingdSQ.sh
+++ docs/parallel_usingdSQ.sh
@@ -12,17 +12,16 @@ job_list="job_list.txt"
 > "$job_list"
 
 # Generate job list
-family="binomial"
-for arg1 in {1..3}; do 
-        for iter in {1..100}; do
-            echo "module load R/4.4.1-foss-2022b; Rscript main.R $arg1 $iter" >> "$job_list"
-        done
+for iter in {0..1193}; do
+    echo "module load miniconda; conda activate py3_env; python main.py --iter $iter"  >> $job_list
+    # OR
+    # echo "module load R/4.4.1-foss-2022b; Rscript main.R $arg1 $iter" >> "$job_list"
 done
 echo "Job list file '$job_list' created successfully."
 
 
 # STEP 2: RUN this to create sbatch file
-dsq --job-file job_list.txt --mem-per-cpu 32g -t 06:00:00 --mail-type ALL
+dsq --job-file job_list.txt --mem-per-cpu 32g -t 02:00:00 --mail-type ALL --status-dir dir_status  --output dir_output/dsq-joblist-%A_%3a-%N.out
 
 
 # STEP 3: submit joblist
